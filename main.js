f6ea239d5f61330a64d32b924ed06d1f3c5a63ac
bejaouifehmi <150477673+bejaouifehmi@users.noreply.github.com>
2023-11-22 19:17:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+function each(coll, f) {
+    if (Array.isArray(coll)) {
+      for (var i = 0; i < coll.length; i++) {
+        f(coll[i], i);
+      }
+    } else {
+      for (var key in coll) {
+        f(coll[key], key);
+      }
+    }
+  }
+  
+  function filter(array, predicate) {
+    var acc = [];
+    each(array, function (element, i) {
+      if (predicate(element, i)) {
+        acc.push(element);
+      }
+    });
+    return acc;
+  }
+  
+  function map(array, func) {
+    var acc = [];
+    each(array, function (element, i) {
+      acc.push(func(element, i));
+    });
+    return acc;
+  }
+  
+  function reduce(array, f, acc) {
+    if (acc === undefined) {
+      acc = array[0];
+      array = array.slice(1);
+    }
+    each(array, function (element, i) {
+      acc = f(acc, element, i);
+    });
+    return acc;
+  }
+product=['Ikea','Geant','the house','Graiet','Darty']
+var nav=document.getElementById('navigator');
+product.map(element=>{
+var list=document.createElement('ul');
+list.innerHTML=`<li>${element}</li>`
+nav.appendChild(list)
+console.log(element)
+})
+console.log(nav);
+
